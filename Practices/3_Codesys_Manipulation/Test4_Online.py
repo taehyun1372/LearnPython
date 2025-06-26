3b42a5de9fcd6fef407fe27f3c656b4918627f08
Roy Choi <roy.choi@edwardsvacuum.com>
2025-06-26 11:51:09 +0900

    [Online Code]

--- Practices/3_Codesys_Manipulation/Test4_Online.py
+++ Practices/3_Codesys_Manipulation/Test4_Online.py
@@ -3,9 +3,12 @@ clr.AddReference("System")  # Needed to access .NET types
 from System.Net import IPAddress
 
 DEVICE_NAME = "Codesys_Control_Win_V3"
-GATEWAY_NAME = "Gateway-9"
-IP_ADDRESS = "172.17.0.4"
-TARGET_PORT = 1217
+GATEWAY_NAME = "Gateway-6"
+GATEWAY_IP_ADDRESS = "192.168.121.132"
+GATEWAY_PORT = 1217
+INSTANCE_IP_ADDRESS = "172.17.0.4"
+INSTANCE_ID = "Instance1"
+INSTANCE_PASSWORD = "Wonderous7641!"
 
 proj = projects.primary
 
@@ -18,28 +21,46 @@ else:
             print(devices[0].get_name())
             device = devices[0]
 
-            ip = IPAddress.Parse(IP_ADDRESS)
+            ip = IPAddress.Parse(INSTANCE_IP_ADDRESS)
 
             gateways = online.gateways
             matching_gateway = False
+            gateway_names = []
+            target_gateway_name = GATEWAY_NAME
             for gateway in gateways:
-                if gateway.name ==  GATEWAY_NAME:
+                gateway_names.append(gateway.name)
+                if (gateway.config_params[0] == GATEWAY_IP_ADDRESS and  # Check ip address
+                        gateway.config_params[1] == GATEWAY_PORT): # Check port
                     matching_gateway = True
+                    target_gateway_name = gateway.name
+
 
             if not matching_gateway:
                 print('No matching gateway found..creating a new gateway')
 
                 params = {
-                    0: IP_ADDRESS,  # or use param.id == 0
-                    1: TARGET_PORT  # or use param.id == 1
+                    0: GATEWAY_IP_ADDRESS,  # or use param.id == 0
+                    1: GATEWAY_PORT  # or use param.id == 1
                 }
 
-                gateways.add_new_gateway(GATEWAY_NAME, params)
+                while(target_gateway_name in gateway_names):
+                    prefix, suffix = target_gateway_name.split('-')
+                    target_gateway_name = prefix + '-' + str(int(suffix) + 1)
+
+                gateways.add_new_gateway(target_gateway_name, params)
 
             try:
-                device.set_gateway_and_ip_address(GATEWAY_NAME, ip)
+                device.set_gateway_and_ip_address(target_gateway_name, ip)
+                online_application = online.create_online_application()
+                online_device = online_application.get_online_device()
+                online.set_specific_credentials(target=online_device, username=INSTANCE_ID, password=INSTANCE_PASSWORD)
+                online_device.connect()
+
+                online_application.login(OnlineChangeOption.Try, True)
+                online_application.start()
+                # online_device.set_credentials_for_initial_user(username="Instance1", password="Wonderous7641!", can_change_password=True, must_change_password=False)
             except Exception as exception:
-                print("Failed to set the target gateway ", GATEWAY_NAME)
+                print("Failed to set the target gateway ", target_gateway_name)
 
 
 
