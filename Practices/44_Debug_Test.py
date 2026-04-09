efc9a1c0234d3a74b91fd893e62e2bd716a1ea77
Taehyun <taehyun1372@naver.com>
2026-04-09 09:51:44 +0900

    Added a function definition

--- Practices/44_Debug_Test.py
+++ Practices/44_Debug_Test.py
@@ -1,6 +1,16 @@
+def batch_print():
+    print("Line 11")
+    print("Line 12")
+    print("Line 13")
+    print("Line 14")
+    print("Line 15")
+    print("Line 16")
+
+
 print("Line 1")
 print("Line 2")
 print("Line 3")
 print("Line 4")
 print("Line 5")
 print("Line 6")
+batch_print()
